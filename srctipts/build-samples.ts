61eea1f1f243a56b5b892b8fa01a469850716ec8
Max Zakharzhevskiy <maxzz@msn.com>
2025-01-26 15:30:23 -0800

    WIP. samples. attach debugger

--- srctipts/build-samples.ts
+++ srctipts/build-samples.ts
@@ -1,22 +1,35 @@
+import path from "path";
 import fs from "fs";
 
 // create a function collect all js file names in the src/assets/samples folder and create single file
 // with all the samples
 
-function collectSamples() {
-    const samples = new Map<string, string>();
+const res = new Map<string, string[]>();
 
-    const files = fs.readdirSync("./src/assets/samples");
+function collectSamples(folder: string) {
+    console.log("folder", folder);
 
-    for (const file of files) {
-        const name = file.replace(".js", "");
-        const content = fs.readFileSync(`./src/assets/samples/${file}`, "utf-8");
-        samples.set(name, content);
+    const fnames = fs.readdirSync(`${folder}`);
+
+    for (const fname of fnames) {
+        const fullname = `${folder}/${fname}`;
+        const st = fs.statSync(fullname);
+
+        if (st.isDirectory()) {
+            collectSamples(path.join(folder, fname));
+            continue;
+        } else if (st.isFile() && fname.endsWith(".js")) {
+            const content = fs.readFileSync(fullname, "utf-8");
+
+            if (!res.has(folder)) {
+                res.set(folder, []);
+            }
+            res.get(folder)?.push(fname);
+        }
     }
-
-    return samples;
 }
 
-const samples = collectSamples();
+const rootFolder = path.resolve("./src/assets/samples");
+collectSamples(rootFolder);
 
-fs.writeFileSync("./src/assets/samples.js", `export default ${JSON.stringify(samples)}`);
+fs.writeFileSync("./src/assets/samples.js", `export default ${JSON.stringify([...res])}`);
