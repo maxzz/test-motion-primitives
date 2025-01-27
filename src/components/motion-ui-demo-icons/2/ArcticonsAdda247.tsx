e65aba464a6f42b2ee27f8468e589792117d157a
Max Zakharzhevskiy <maxzz@msn.com>
2025-01-26 19:20:31 -0800

    WIP. samples. fix errors. 2. add missing icons

---
+++ src/components/motion-ui-demo-icons/2/ArcticonsAdda247.tsx
@@ -0,0 +1,7 @@
+import React, { SVGProps } from 'react'
+
+export function A24(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 48 48" {...props}><path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" d="M39.445 43.451L24.854 4.6l-15.129 39c10.757-6.374 19.775-6.374 29.72-.149"></path></svg>
+  )
+}
---
+++ src/components/motion-ui-demo-icons/2/ArcticonsStrava.tsx
@@ -0,0 +1,7 @@
+import React, { SVGProps } from 'react'
+
+export function Strava(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 48 48" {...props}><path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" d="M31.016 26.855L19.839 4.5L8.661 26.855m14.033 0L31.016 43.5l8.323-16.645"></path></svg>
+  )
+}
