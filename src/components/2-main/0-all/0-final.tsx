c095ce3eb009906cff5e1816e6eda539ca0f7f2b
Max Zakharzhevskiy <maxzz@msn.com>
2025-01-27 20:03:20 -0800

    WIP. samples. view. connect render of fileCnt and component

--- src/components/2-main/0-all/0-final.tsx
+++ src/components/2-main/0-all/0-final.tsx
@@ -1,10 +1,27 @@
 import { ScrollArea } from "@/ui/shadcn";
-import { allSamples } from "@/assets/generated/dev/all-samples-2";
+import { allSamples, type ExportItem } from "@/assets/generated/dev/all-samples-2";
 import { lastFname } from "@/utils/utils-os";
+import { proxy, useSnapshot } from "valtio";
+import { classNames } from "@/utils";
+
+function findExportItem(uuid: number): ExportItem | undefined {
+    if (!uuid) {
+        return;
+    }
+    for (const [key, value] of Object.entries(allSamples)) {
+        for (const item of value) {
+            if (item.uuid === uuid) {
+                return item;
+            }
+        }
+    }
+}
+
+const editor = proxy({ uuid: 0 });
 
 export function Main() {
     return (
-        <main className="p-4 grid grid-cols-2 debug">
+        <main className="p-4 grid grid-cols-2 1debug">
             <ComponentsList />
 
             <div className="flex flex-col gap-4">
@@ -18,18 +35,23 @@ export function Main() {
 
 function ComponentsList() {
     return (
-        <div className="bg-green-200">
+        <div className="px-4 py-2 bg-green-200">
             {Object.entries(allSamples).map(
                 ([key, value]) => {
                     return (
                         <div key={key}>
                             <h2 className="text-xs font-bold">{key}</h2>
-                            <ul className="px-4 text-xs flex flex-col gap-1">
+                            <ul className="px-4 text-xs flex flex-col">
                                 {value.map(
-                                    (item, idx) => {
+                                    (item: ExportItem, idx: number) => {
+                                        const isSelected = useSnapshot(editor).uuid === item.uuid;
                                         const text = lastFname(item.fullname);
                                         return (
-                                            <li className="" key={idx}>
+                                            <li
+                                                className={classNames("py-0.5 cursor-pointer", isSelected && "bg-green-600 font-semibold")}
+                                                onClick={() => editor.uuid = item.uuid}
+                                                key={item.uuid}
+                                            >
                                                 {text}
                                             </li>
                                         );
@@ -45,11 +67,16 @@ function ComponentsList() {
 }
 
 function DemoScrollArea() {
+    const uuid = useSnapshot(editor).uuid;
+    const item = findExportItem(uuid);
     return (
         <div className="flex flex-col gap-4 bg-green-300">
             <ScrollArea className="w-full h-96">
                 <div className="h-full w-full flex flex-col gap-4">
-                    2
+                    {item
+                        ? <item.component />
+                        : uuid
+                    }
                 </div>
             </ScrollArea>
         </div>
@@ -57,11 +84,13 @@ function DemoScrollArea() {
 }
 
 function SourceCodeArea() {
+    const uuid = useSnapshot(editor).uuid;
+    const item = findExportItem(uuid);
     return (
         <div className="flex flex-col gap-4 bg-green-400">
             <ScrollArea className="h-96 w-full">
                 <div className="h-full w-full flex flex-col gap-4">
-                    3
+                    {item?.fileCnt}
                 </div>
             </ScrollArea>
         </div>
