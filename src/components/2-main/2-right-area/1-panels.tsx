fe4cc45669abd18e7b4355123b321af1858b8399
Max Zakharzhevskiy <maxzz@msn.com>
2025-01-28 19:19:01 -0800

    WIP. samples. view. connect SourceCodeDrawer

--- src/components/2-main/2-right-area/1-panels.tsx
+++ src/components/2-main/2-right-area/1-panels.tsx
@@ -6,6 +6,8 @@ import { ScrollArea } from "@/ui/shadcn";
 import { editor } from "../0-all/8-editor-state";
 import { CodeHighlighter } from "./8-code-highlighter";
 import { findExportItem } from "./8-find-item";
+import { Button } from "@/ui/button";
+import { SourceCodeDrawer } from "./2-drawer";
 
 export function RightArea({ className }: ComponentPropsWithRef<"div">) {
     const uuid = useSnapshot(editor).uuid;
@@ -13,14 +15,18 @@ export function RightArea({ className }: ComponentPropsWithRef<"div">) {
     return (
         <div className={classNames("relative w-full h-full", className)}>
             <DemoScrollArea uuid={uuid} item={item} />
-            {/* <SourceCodeArea uuid={uuid} item={item} /> */}
+            <SourceCodeArea uuid={uuid} item={item} />
         </div>
     );
 }
 
 export function DemoScrollArea({ uuid, item }: { uuid: number | undefined; item: ExportItem | undefined; }) {
     return (
-        <div className="flex flex-col gap-4 bg-green-300">
+        <div className="relative flex flex-col gap-4 bg-green-300">
+            <Button className="absolute left-4 top-4" variant="outline" size="icon" onClick={() => editor.isDrawerOpen = true}>
+                Code
+            </Button>
+
             <ScrollArea className="w-full h-96">
                 <div className="h-full w-full flex flex-col gap-4">
                     {item
@@ -35,12 +41,14 @@ export function DemoScrollArea({ uuid, item }: { uuid: number | undefined; item:
 
 export function SourceCodeArea({ uuid, item }: { uuid: number | undefined; item: ExportItem | undefined; }) {
     return (
-        <div className="flex flex-col gap-4 bg-green-400">
-            <ScrollArea className="h-96 w-full">
-                <div className="h-full w-full text-xs whitespace-pre-wrap">
-                    <CodeHighlighter code={item?.fileCnt} />
-                </div>
-            </ScrollArea>
-        </div>
+        <SourceCodeDrawer>
+            <div className="bg-purple-400">
+                <ScrollArea className="h-96 w-full">
+                    <div className="h-full w-full text-xs whitespace-pre-wrap">
+                        <CodeHighlighter code={item?.fileCnt} />
+                    </div>
+                </ScrollArea>
+            </div>
+        </SourceCodeDrawer>
     );
 }
---
+++ src/ui/shadcn/button.tsx
@@ -0,0 +1,58 @@
+import { ButtonHTMLAttributes, forwardRef } from "react";
+import { Slot } from "@radix-ui/react-slot";
+import { cva, type VariantProps } from "class-variance-authority";
+import { cn } from "@/utils";
+
+const defaultVariantClasses = "\
+text-sm font-medium \
+\
+focus-visible:ring-1 \
+focus-visible:ring-ring \
+focus-visible:outline-none \
+\
+disabled:opacity-50 \
+disabled:pointer-events-none \
+\
+transition-colors \
+rounded-md select-none \
+inline-flex items-center justify-center";
+
+const buttonVariantsClasses = cva(defaultVariantClasses, {
+    variants: {
+        variant: {
+            default: "bg-primary text-primary-foreground hover:bg-primary/90 shadow",
+            destructive: "bg-destructive text-destructive-foreground hover:bg-destructive/90 shadow-sm",
+            outline: "border border-input bg-transparent hover:bg-accent hover:text-accent-foreground shadow-sm",
+            secondary: "bg-secondary text-secondary-foreground hover:bg-secondary/80 shadow-sm",
+            ghost: "hover:bg-accent hover:text-accent-foreground",
+            link: "text-primary underline-offset-4 hover:underline",
+        },
+        size: {
+            default: "px-4 py-2 h-9",
+            xs: "px-2 h-7 text-xs rounded-md",
+            sm: "px-3 h-8 text-xs rounded-md",
+            lg: "px-8 h-10 rounded-md",
+            icon: "size-9",
+        },
+    },
+    defaultVariants: {
+        variant: "outline",
+        size: "xs",
+    },
+});
+
+export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement>, VariantProps<typeof buttonVariantsClasses> {
+    asChild?: boolean;
+}
+
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+    ({ className, variant, size, asChild = false, ...rest }, ref) => {
+        const Comp = asChild ? Slot : "button";
+        return (
+            <Comp className={cn(buttonVariantsClasses({ variant, size, className }))} ref={ref} {...rest} />
+        );
+    }
+);
+Button.displayName = "Button";
+
+export { Button, buttonVariantsClasses as buttonVariants };
