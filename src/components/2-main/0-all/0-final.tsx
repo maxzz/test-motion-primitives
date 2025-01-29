416277796c1034cfb632a077839332b709e9ba10
Max Zakharzhevskiy <maxzz@msn.com>
2025-01-28 18:31:50 -0800

    WIP. samples. view. split main monolith. 3

--- src/components/2-main/0-all/0-final.tsx
+++ src/components/2-main/0-all/0-final.tsx
@@ -1,16 +1,11 @@
 import { ComponentsList } from "./1-components-list";
-import { DemoScrollArea, SourceCodeArea } from "../2-right-area";
+import { RightArea } from "../2-right-area";
 
 export function Main() {
     return (
         <main className="p-4 grid grid-cols-[auto_1fr] 1debug">
             <ComponentsList />
-
-            <div className="flex flex-col gap-4">
-                <DemoScrollArea />
-                <SourceCodeArea />
-            </div>
-
+            <RightArea className="flex flex-col gap-4" />
         </main>
     );
 }
--- src/components/2-main/2-right-area/1-panels.tsx
+++ src/components/2-main/2-right-area/1-panels.tsx
@@ -1,8 +1,19 @@
+import { type ComponentProps } from "react";
+import { useSnapshot } from "valtio";
+import { classNames } from "@/utils";
 import { ScrollArea } from "@/ui/shadcn";
 import { allSamples, type ExportItem } from "@/assets/generated/dev/all-samples-2";
-import { useSnapshot } from "valtio";
 import { CodeHighlighter } from "./8-code-highlighter";
 import { editor } from "../0-all/8-editor-state";
+
+export function RightArea({className}: ComponentProps<"div">) {
+    return (
+        <div className={classNames("flex flex-col gap-4", className)}>
+            <DemoScrollArea />
+            <SourceCodeArea />
+        </div>
+    );
+}
 
 function findExportItem(uuid: number): ExportItem | undefined {
     if (!uuid) {
