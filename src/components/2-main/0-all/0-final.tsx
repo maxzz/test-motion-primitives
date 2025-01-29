7b55065c9197870733290d2a354c60a7c21f5b17
Max Zakharzhevskiy <maxzz@msn.com>
2025-01-28 18:28:07 -0800

    WIP. samples. view. split main monolith. 2

--- src/components/2-main/0-all/0-final.tsx
+++ src/components/2-main/0-all/0-final.tsx
@@ -1,7 +1,3 @@
-import { ScrollArea } from "@/ui/shadcn";
-import { allSamples, type ExportItem } from "@/assets/generated/dev/all-samples-2";
-import { useSnapshot } from "valtio";
-import { CodeHighlighter } from "../2-right-area/8-code-highlighter";
 import { ComponentsList } from "./1-components-list";
 import { DemoScrollArea, SourceCodeArea } from "../2-right-area";
 
