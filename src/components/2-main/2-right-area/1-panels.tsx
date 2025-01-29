fa6673682113c22f557e0c8669220e4ed90e6068
Max Zakharzhevskiy <maxzz@msn.com>
2025-01-28 18:44:28 -0800

    WIP. samples. view. split main monolith. 5

--- src/components/2-main/2-right-area/1-panels.tsx
+++ src/components/2-main/2-right-area/1-panels.tsx
@@ -1,23 +1,24 @@
 import { type ComponentPropsWithRef } from "react";
 import { useSnapshot } from "valtio";
 import { classNames } from "@/utils";
+import { type ExportItem } from "@/assets/generated/dev/all-samples-2";
 import { ScrollArea } from "@/ui/shadcn";
 import { editor } from "../0-all/8-editor-state";
 import { CodeHighlighter } from "./8-code-highlighter";
 import { findExportItem } from "./8-find-item";
 
 export function RightArea({ className }: ComponentPropsWithRef<"div">) {
+    const uuid = useSnapshot(editor).uuid;
+    const item = findExportItem(uuid);
     return (
         <div className={classNames("flex flex-col gap-4", className)}>
-            <DemoScrollArea />
-            <SourceCodeArea />
+            <DemoScrollArea uuid={uuid} item={item} />
+            <SourceCodeArea uuid={uuid} item={item} />
         </div>
     );
 }
 
-export function DemoScrollArea() {
-    const uuid = useSnapshot(editor).uuid;
-    const item = findExportItem(uuid);
+export function DemoScrollArea({ uuid, item }: { uuid: number | undefined; item: ExportItem | undefined; }) {
     return (
         <div className="flex flex-col gap-4 bg-green-300">
             <ScrollArea className="w-full h-96">
@@ -32,9 +33,7 @@ export function DemoScrollArea() {
     );
 }
 
-export function SourceCodeArea() {
-    const uuid = useSnapshot(editor).uuid;
-    const item = findExportItem(uuid);
+export function SourceCodeArea({ uuid, item }: { uuid: number | undefined; item: ExportItem | undefined; }) {
     return (
         <div className="flex flex-col gap-4 bg-green-400">
             <ScrollArea className="h-96 w-full">
