4db8f4420c81fa4fb2e9146c86208ebb4d57fa37
Max Zakharzhevskiy <maxzz@msn.com>
2025-01-29 19:29:15 -0800

    WIP. samples. view. fix DemoSourceSwitcher active state indicator

--- src/components/2-main/2-right-area/4-demo-code-switcher.tsx
+++ src/components/2-main/2-right-area/4-demo-code-switcher.tsx
@@ -9,13 +9,13 @@ export function DemoSourceSwitcher({ className, ...rest }: ComponentPropsWithRef
     return (
         <div className={classNames("flex items-center gap-1", className)} {...rest}>
             <Button
-                className={classNames("px-0 hover:no-underline", !isDrawerOpen && "underline")} variant="link" size="xs"
+                className={classNames("px-0", !isDrawerOpen ? "underline" : "hover:no-underline")} variant="link" size="xs"
                 onClick={() => editor.isDrawerOpen = false}
             >
                 Demo
             </Button>
             <Button
-                className={classNames("px-0 hover:no-underline", isDrawerOpen && "underline")} variant="link" size="xs"
+                className={classNames("px-0", isDrawerOpen ? "underline" : "hover:no-underline")} variant="link" size="xs"
                 onClick={() => editor.isDrawerOpen = true}
             >
                 Code
