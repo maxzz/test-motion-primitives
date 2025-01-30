91a05af616db765ba220d5b11dc979aeb3f00d3c
Max Zakharzhevskiy <maxzz@msn.com>
2025-01-29 19:17:29 -0800

    WIP. samples. view. add DemoSourceSwitcher

--- src/components/2-main/2-right-area/2-demo-area.tsx
+++ src/components/2-main/2-right-area/2-demo-area.tsx
@@ -1,6 +1,7 @@
+import { useSnapshot } from "valtio";
 import { type ExportItem } from "@/assets/generated/dev/all-samples-2";
-import { ScrollArea } from "@/ui/shadcn";
 import { editor } from "../0-all/8-editor-state";
+import { ScrollArea } from "@/ui/shadcn";
 import { Button } from "@/ui/shadcn";
 import { classNames } from "@/utils";
 
@@ -17,28 +18,7 @@ export function DemoScrollArea({ uuid, item }: { uuid: number | undefined; item:
                 Code
             </Button> */}
 
-            {/* 2 buttons to switch between demo and source code */}
-            <div className="absolute left-4 top-4 z-10 flex items-center gap-1">
-                <Button
-                    className={classNames("px-0")} variant="link" size="xs"
-                    onClick={() => {
-                        editor.isDrawerOpen = true;
-                        console.log('isDrawerOpen', editor.isDrawerOpen);
-                    }}
-                >
-                    Demo
-                </Button>
-                <Button
-                    className={classNames("px-0")} variant="link" size="xs"
-                    onClick={() => {
-                        editor.isDrawerOpen = true;
-                        console.log('isDrawerOpen', editor.isDrawerOpen);
-                    }}
-                >
-                    Code
-                </Button>
-            </div>
-
+            <DemoSourceSwitcher />
 
             <div className="relative w-full h-full">
                 <div className="absolute inset-0">
@@ -52,6 +32,32 @@ export function DemoScrollArea({ uuid, item }: { uuid: number | undefined; item:
                     </ScrollArea>
                 </div>
             </div>
+        </div>
+    );
+}
+
+export function DemoSourceSwitcher() {
+    const isDrawerOpen = useSnapshot(editor).isDrawerOpen;
+    return (
+        <div className="absolute left-4 top-4 z-10 flex items-center gap-1">
+            <Button
+                className={classNames("px-0", isDrawerOpen && "underline")} variant="link" size="xs"
+                onClick={() => {
+                    editor.isDrawerOpen = true;
+                    console.log('isDrawerOpen', editor.isDrawerOpen);
+                }}
+            >
+                Demo
+            </Button>
+            <Button
+                className={classNames("px-0", isDrawerOpen && "underline")} variant="link" size="xs"
+                onClick={() => {
+                    editor.isDrawerOpen = true;
+                    console.log('isDrawerOpen', editor.isDrawerOpen);
+                }}
+            >
+                Code
+            </Button>
         </div>
     );
 }
