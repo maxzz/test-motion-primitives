9022ca93977b069fb4f971b98ed6c651dc3433c1
Max Zakharzhevskiy <maxzz@msn.com>
2025-01-30 14:20:58 -0800

    WIP. samples. formatting. 2

--- src/assets/samples/CTA Sections/2.tsx
+++ src/assets/samples/CTA Sections/2.tsx
@@ -1,48 +1,48 @@
 'use client';
-import { ArrowRight } from 'lucide-react';
-import { TextEffect } from "@/components/motion-ui/text-effect";
 import { useRef } from 'react';
 import { useInView } from 'motion/react';
+import { TextEffect } from "@/components/motion-ui/text-effect";
+import { ArrowRight } from 'lucide-react';
 
 export function Cta2() {
-  const ref = useRef(null);
-  const isInView = useInView(ref);
+    const ref = useRef(null);
+    const isInView = useInView(ref);
+    return (
+        <div className='py-24 sm:py-32' ref={ref}>
+            <div className='mx-auto max-w-7xl px-6 lg:px-8'>
+                <div className='relative text-center  flex flex-col md:flex-row items-start justify-between space-y-4 md:items-center md:space-y-0'>
 
-  return (
-    <div className='py-24 sm:py-32' ref={ref}>
-      <div className='mx-auto max-w-7xl px-6 lg:px-8'>
-        <div className='relative flex flex-col items-start justify-between space-y-4 text-center md:flex-row md:items-center md:space-y-0'>
-          <div className='flex flex-col space-y-2 text-left md:space-y-4'>
-            <TextEffect
-              className='text-3xl text-zinc-900 dark:text-white'
-              as='h2'
-              preset='blur'
-              trigger={isInView}
-              per='char'
-            >
-              Join the community
-            </TextEffect>
-            <p className='text-zinc-500 dark:text-zinc-400'>
-              More than +1000 designers sharing their work.
-            </p>
-          </div>
-          <div className='flex flex-row items-center space-x-4'>
-            <a
-              href='#'
-              className='inline-flex items-center rounded-md bg-zinc-900 px-2.5 py-1.5 text-sm text-white transition-colors duration-300 hover:bg-zinc-700'
-            >
-              Join now
-            </a>
-            <a
-              href='#'
-              className='inline-flex items-center gap-1 rounded-md bg-transparent px-2.5 py-1.5 text-sm text-zinc-900 transition-colors duration-300 hover:bg-zinc-100 dark:text-white dark:hover:bg-zinc-800'
-            >
-              Learn More
-              <ArrowRight className='h-4 w-4' />
-            </a>
-          </div>
+                    <div className='text-left flex flex-col space-y-2 md:space-y-4'>
+
+                        <TextEffect
+                            className='text-3xl text-zinc-900 dark:text-white'
+                            as='h2'
+                            preset='blur'
+                            trigger={isInView}
+                            per='char'
+                        >
+                            Join the community
+                        </TextEffect>
+
+                        <p className='text-zinc-500 dark:text-zinc-400'>
+                            More than +1000 designers sharing their work.
+                        </p>
+                    </div>
+
+                    <div className='flex flex-row items-center space-x-4'>
+
+                        <a href='#' className='inline-flex items-center rounded-md bg-zinc-900 px-2.5 py-1.5 text-sm text-white transition-colors duration-300 hover:bg-zinc-700' >
+                            Join now
+                        </a>
+
+                        <a href='#' className='inline-flex items-center gap-1 rounded-md bg-transparent px-2.5 py-1.5 text-sm text-zinc-900 transition-colors duration-300 hover:bg-zinc-100 dark:text-white dark:hover:bg-zinc-800'>
+                            Learn More
+                            <ArrowRight className='size-4' />
+                        </a>
+
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
--- src/assets/samples/CTA Sections/3.tsx
+++ src/assets/samples/CTA Sections/3.tsx
@@ -1,52 +1,44 @@
 'use client';
+import { useRef, useState } from 'react';
+import { useInView } from 'motion/react';
 import { ArrowRightIcon } from 'lucide-react';
 import { AnimatedNumber } from "@/components/motion-ui/animated-number";
-import { useInView } from 'motion/react';
-import { useRef, useState } from 'react';
 
 export function Cta3() {
-  const [value, setValue] = useState(0);
-  const ref = useRef(null);
-  const isInView = useInView(ref);
+    const [value, setValue] = useState(0);
+    const isInViewRef = useRef(null);
+    const isInView = useInView(isInViewRef);
 
-  if (isInView && value === 0) {
-    setValue(10000);
-  }
+    if (isInView && value === 0) {
+        setValue(10000);
+    }
 
-  return (
-    <div
-      className='mx-auto max-w-screen-xl px-4 py-8 sm:py-16 lg:px-6'
-      ref={ref}
-    >
-      <div className='relative mx-auto max-w-screen-sm py-12 text-center'>
-        <div className='relative'>
-          <h2 className='mb-4 text-3xl text-zinc-900 dark:text-white'>
-            Join the community
-          </h2>
-          <p className='mb-6 text-zinc-500 dark:text-zinc-400'>
-            More than +
-            <AnimatedNumber
-              value={value}
-              springOptions={{ bounce: 0, duration: 2000 }}
-            />{' '}
-            designers sharing their work.
-          </p>
-          <div className='flex items-center justify-center gap-4'>
-            <a
-              href='#'
-              className='inline-flex items-center rounded-md bg-zinc-900 px-2.5 py-1.5 text-sm text-white transition-colors duration-300 hover:bg-zinc-700 dark:bg-white dark:text-zinc-900 dark:hover:bg-zinc-200'
-            >
-              Join now
-            </a>
-            <a
-              href='#'
-              className='inline-flex items-center gap-1 rounded-md bg-transparent px-2.5 py-1.5 text-sm text-zinc-900 transition-colors duration-300 hover:bg-zinc-200 dark:text-zinc-50 dark:hover:bg-zinc-900'
-            >
-              Learn more <ArrowRightIcon className='h-4 w-4' />
-            </a>
-          </div>
+    return (
+        <div ref={isInViewRef} className='mx-auto max-w-screen-xl px-4 py-8 sm:py-16 lg:px-6'>
+            <div className='relative mx-auto max-w-screen-sm py-12 text-center'>
+
+                <h2 className='mb-4 text-3xl text-zinc-900 dark:text-white'>
+                    Join the community
+                </h2>
+
+                <p className='mb-6 text-zinc-500 dark:text-zinc-400'>
+                    More than +{' '}
+
+                    <AnimatedNumber value={value} springOptions={{ bounce: 0, duration: 2000 }} />
+
+                    {' '}designers sharing their work.
+                </p>
+
+                <div className='flex items-center justify-center gap-4'>
+                    <a href='#' className='inline-flex items-center rounded-md bg-zinc-900 px-2.5 py-1.5 text-sm text-white transition-colors duration-300 hover:bg-zinc-700 dark:bg-white dark:text-zinc-900 dark:hover:bg-zinc-200'>
+                        Join now
+                    </a>
+                    <a href='#' className='inline-flex items-center gap-1 rounded-md bg-transparent px-2.5 py-1.5 text-sm text-zinc-900 transition-colors duration-300 hover:bg-zinc-200 dark:text-zinc-50 dark:hover:bg-zinc-900'>
+                        Learn more <ArrowRightIcon className='h-4 w-4' />
+                    </a>
+                </div>
+
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
