e2b494707a3179b5da5005978b67f8ee8d3fed87
Max Zakharzhevskiy <maxzz@msn.com>
2025-01-30 16:57:59 -0800

    WIP. build. add manual chunks

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,12 @@ import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+function manualChunks(id: string) { //https://rollupjs.org/configuration-options/#output-manualchunks
+    if (id.includes("all-samples-2")) {
+        return "samples";
+    }
+}
+
 export default defineConfig({ // https://vite.dev/config/
     plugins: [
         react(),
@@ -9,6 +15,13 @@ export default defineConfig({ // https://vite.dev/config/
     resolve: {
         alias: {
             '@': resolve(__dirname, './src'),
+        },
+    },
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks,
+            },
         },
     },
     server: {
